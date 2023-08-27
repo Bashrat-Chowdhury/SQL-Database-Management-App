b1c591c52cc162ff5a532dfaee7380ce66439865
bashratchy <bashrat03@gmail.com>
2023-08-27 18:49:19 -0400

    added schema.sql for the app

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+  id INT NOT NULL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+  id INT NOT NULL PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  slary DECIMAL NOT NULL,
+  department_id INT NOT NULL 
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL 
+  manager_id INT NOT NULL
+  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
+  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
+
