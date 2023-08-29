79f0fb638359333636c437b221ece157a5236694
bashratchy <bashrat03@gmail.com>
2023-08-29 19:20:26 -0400

    Added comments to code

--- db/connection.js
+++ db/connection.js
@@ -8,9 +8,11 @@ const connection = mysql.createConnection({
   database: "employees_db",
 });
 
+//error messaging if connection fails
 connection.connect((err) => {
   if (err) throw err;
   console.log("Connected to the database.");
 });
 
+//importing connection function
 module.exports = connection;
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const { prompt } = require("inquirer");
+//sql connection requirement
 const db = require("./db/connection");
 
+//calling function for initial prompts
 initialprompts();
 
+//function for initial prompts for user to interact with employee data
 function initialprompts() {
   prompt([
     {
@@ -42,6 +45,7 @@ function initialprompts() {
     },
   ]).then((res) => {
     let choice = res.initiallist;
+    //different functions called for each case of user action
     switch (choice) {
       case "VIEW_EMPLOYEES":
         viewEmployees();
@@ -68,6 +72,7 @@ function initialprompts() {
   });
 }
 
+//function using SQL query to view employee table
 function viewEmployees() {
   const query = "SELECT * FROM employee";
   db.query(query, (err, res) => {
@@ -77,6 +82,7 @@ function viewEmployees() {
   });
 }
 
+//function using SQL query to view role table
 function viewRoles() {
   const query = "SELECT * FROM role";
   db.query(query, (err, res) => {
@@ -86,6 +92,7 @@ function viewRoles() {
   });
 }
 
+//function using SQL query to view department table
 function viewDeps() {
   const query = "SELECT * FROM department";
   db.query(query, (err, res) => {
@@ -96,11 +103,13 @@ function viewDeps() {
 }
 
 function addDepartment() {
+  //function prompts further questions to user
   prompt([
     {
       name: "name",
       message: "What is the name of the department?",
     },
+    //user input used to add new department to table using another function
   ]).then((res) => {
     let departmentName = res.name;
     createDepartment(departmentName)
@@ -115,6 +124,7 @@ function addDepartment() {
   });
 }
 
+//user input used as values to add new row to department table
 function createDepartment(departmentName) {
   return new Promise((resolve, reject) => {
     const query = "INSERT INTO department (name) VALUES (?)";
@@ -129,6 +139,7 @@ function createDepartment(departmentName) {
 }
 
 function addRole() {
+  //function prompts further questions to user
   prompt([
     {
       name: "title",
@@ -142,12 +153,14 @@ function addRole() {
       name: "department_id",
       message: "Enter the department ID for the new role:",
     },
+    //user input used to add new role to table using another function
   ]).then((res) => {
     const { title, salary, department_id } = res;
     createRole(title, salary, department_id);
   });
 }
 
+//user input used as values to add new row to role table
 function createRole(title, salary, department_id) {
   const query =
     "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)";
@@ -162,6 +175,7 @@ function createRole(title, salary, department_id) {
 }
 
 function addEmployee() {
+  //function prompts further questions to user
   prompt([
     {
       name: "first_name",
@@ -179,12 +193,14 @@ function addEmployee() {
       name: "manager_id",
       message: "Enter the manager ID for the new employee:",
     },
+    //user input used to add new employee to table using another function
   ]).then((res) => {
     const { first_name, last_name, role_id, manager_id } = res;
     createEmployee(first_name, last_name, role_id, manager_id);
   });
 }
 
+//user input used as values to add new row to employee table
 function createEmployee(first_name, last_name, role_id, manager_id) {
   const query =
     "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)";
@@ -205,6 +221,7 @@ function createEmployee(first_name, last_name, role_id, manager_id) {
 }
 
 function updateEmployeeRole() {
+  //function prompts further questions to user
   prompt([
     {
       name: "employee_id",
@@ -214,12 +231,14 @@ function updateEmployeeRole() {
       name: "new_role_id",
       message: "Enter the new role ID for the employee:",
     },
+    //user input used to update employee role using another function
   ]).then((res) => {
     const { employee_id, new_role_id } = res;
     changeRole(employee_id, new_role_id);
   });
 }
 
+//user input used as values to add change role id for employee
 function changeRole(employee_id, new_role_id) {
   const query = "UPDATE employee SET role_id = ? WHERE id = ?";
   db.query(query, [new_role_id, employee_id], (err, result) => {
